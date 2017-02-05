18f09f5266f19e213d7a29b21db753b1272c0efa
sulinx <wangling2142@live.com>
2017-02-06 01:50:26 +0800

    commit2

--- kcsapi_py.py
+++ kcsapi_py.py
@@ -1,30 +1,58 @@
-from flask import Flask,request,make_response,render_template
+from flask import Flask,request,make_response,render_template,abort
+from flask_sqlalchemy import SQLAlchemy
 from svdata import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/kancolle'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/api_start2',methods=[ 'POST'])
+class Userdata(db.Model):
+    uid = db.Column(db.String(32), primary_key=True)
+    api_material = db.Column(db.Text, unique=True)
+    api_deck_port = db.Column(db.Text, unique=True)
+    api_ndock = db.Column(db.Text, unique=True)
+    api_ship = db.Column(db.Text, unique=True)
+    api_basic = db.Column(db.Text, unique=True)
+    api_log = db.Column(db.Text, unique=True)
+    api_p_bgm_id = db.Column(db.Integer, unique=True)
+    api_kdock = db.Column(db.Text, unique=True)
+    shipnum = db.Column(db.Integer, unique=True)
+    slot_item = db.Column(db.Text, unique=True)
+    slotnum = db.Column(db.Integer, unique=True)
+
+    def __init__(self, username, email):
+        self.uid = username
+
+    def __repr__(self):
+        return '<User %r>' % self.uid
+
+@app.route('/api_start2',methods=['GET', 'POST'])
 def start():
-    if request.form[api_token]:
+    if request.form['api_token']:
         resp = make_response(svdata, 200)
         resp.headers['Content-Type'] = 'application/json;charset=utf-8'
         return resp
     else:
-        abort(401)
+        abort(404)
 
 
-@app.route('/api_get_member/<funcm>',methods=[ 'POST'])
+@app.route('/api_get_member/<funcm>',methods=['GET', 'POST'])
 def api_get_member(funcm):
-    if request.form[api_token]:
+    if request.form['api_token']:
         resp = make_response(svdata, 200)
         resp.headers['Content-Type'] = 'application/json;charset=utf-8'
         return resp
     else:
-        abort(401)
-@app.route('/api_port/port',methods=[ 'POST'])
+        abort(404)
+@app.route('/api_port/port',methods=['GET', 'POST'])
 def api_port():
-    pass
+    a= Userdata.query.all()
+    lis = ''
+    for user in a:
+        lis = lis + '<div>' + user.uid + '</div>'
+    return lis
 @app.route('/api_req_furniture/<funcf>',methods=[ 'POST'])
 def api_req_furniture(funcf):
     pass
